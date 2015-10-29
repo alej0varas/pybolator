46ec67999195a7311232bcac0d1862572fd1dffe
Alejandro Varas <alej0varas@gmail.com>
2015-10-29 13:58:27 +0100

    Added command line option to disable GUI

--- pybolator.py
+++ pybolator.py
@@ -1,4 +1,15 @@
-import pyboard
-import pybgui
+import argparse
 
-pybgui.main(pyboard)
+import pybgui
+import pyboard
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch', action='store_true', default=False,
+                    help="Disable GUI and run main immediately")
+args = parser.parse_args()
+
+if not args.batch:
+    pybgui.main(pyboard)
+else:
+    pyboard._run_code()
